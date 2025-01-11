ea5ed05282265cb6d541561c4c6a37372153f05e
Debanjan <91557339+debanjan1309@users.noreply.github.com>
2025-01-12 02:37:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -89,5 +89,5 @@ def get_pivot():
         return jsonify({"error": "Stock not found"})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 4000))
     app.run(host="0.0.0.0", port=port)
